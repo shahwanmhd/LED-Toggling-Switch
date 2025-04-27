341f0f1ab0750713ba6876963a3b7395926ed2a8
shahwan muhammad <shahwanmuhammad77@gmail.com>
2025-04-27 18:18:45 +0530

    Update in main.c. Added appropriate comments

--- Src/main.c
+++ Src/main.c
@@ -1,3 +1,20 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Shahwan Muhammad
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+
 #include <stdint.h>
 #include "main.h"
 
